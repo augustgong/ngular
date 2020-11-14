3face630aecf21a9c62248f66746e90e3ba01f5c
tazgong <tazgong@gmail.com>
2020-11-15 02:22:29 +0900

    improve readability

--- libs/screen/src/lib/core/breakpoints/break-points-token.ts
+++ libs/screen/src/lib/core/breakpoints/break-points-token.ts
@@ -7,28 +7,32 @@
  */
 
 import {inject, InjectionToken} from '@angular/core';
-import {BreakPoint} from './break-point';
-import {BREAKPOINT} from '../tokens/breakpoint-token';
-import {DEFAULT_BREAKPOINTS} from '../breakpoints/data/break-points';
-import {ORIENTATION_BREAKPOINTS} from '../breakpoints/data/orientation-break-points';
+import {/*interface*/BreakPoint} from './break-point';
+import {/*InjectionToken*/BREAKPOINT} from '../tokens/breakpoint-token';
+import {/*InjectionToken*/LAYOUT_CONFIG} from '../tokens/library-config';
+import {/*Data*/DEFAULT_BREAKPOINTS} from '../breakpoints/data/break-points';
+import {/*Data*/ORIENTATION_BREAKPOINTS} from '../breakpoints/data/orientation-break-points';
 import {mergeByAlias} from '../breakpoints/breakpoint-tools';
-import {LAYOUT_CONFIG} from '../tokens/library-config';
 
 /**
  *  Injection token unique to the flex-layout library.
  *  Use this token when build a custom provider (see below).
  */
-export const BREAKPOINTS =
-  new InjectionToken<BreakPoint[]>('Token (@angular/flex-layout) Breakpoints', {
+export const BREAKPOINTS = new InjectionToken<BreakPoint[]>(
+  'Token (@ngular/screen) Breakpoints',
+  {
     providedIn: 'root',
     factory: () => {
       const breakpoints: any = inject(BREAKPOINT);
       const layoutConfig = inject(LAYOUT_CONFIG);
-      const bpFlattenArray: BreakPoint[] = [].concat.apply([], (breakpoints || [])
-        .map((v: BreakPoint | BreakPoint[]) => Array.isArray(v) ? v : [v]));
+      const bpFlattenArray: BreakPoint[] = [].concat.apply(
+        [],
+        (breakpoints || []).map((v: BreakPoint | BreakPoint[]) => Array.isArray(v) ? v : [v])
+      );
       const builtIns = (layoutConfig.disableDefaultBps ? [] : DEFAULT_BREAKPOINTS)
-        .concat(layoutConfig.addOrientationBps ? ORIENTATION_BREAKPOINTS : []);
-
+                       .concat(layoutConfig.addOrientationBps ? ORIENTATION_BREAKPOINTS : []);
+      
       return mergeByAlias(builtIns, bpFlattenArray);
     }
-  });
+  }
+);
